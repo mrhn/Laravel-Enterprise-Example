cc23a96b35de4f1303c519a6d6ab795c79cbefe2
Martin Henriksen <martin.henriksen.92@gmail.com>
2018-12-16 09:17:27 +0100

    add initial version of controllers

---
+++ app/Http/Controllers/V1/SetController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class SetController extends Controller
+{
+    public function index(): void
+    {
+
+    }
+
+    public function show(): void
+    {
+
+    }
+
+    public function store(): void
+    {
+
+    }
+
+    public function update(): void
+    {
+
+    }
+
+    public function destroy(): void
+    {
+
+    }
+}
